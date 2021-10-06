c547945ea6fae103fca97110b78f11bd8b6e2c17
devajagan <90674727+devajagan@users.noreply.github.com>
2021-10-06 13:35:30 +0530

    Box commit

---
+++ src/ClassDemo.java
@@ -0,0 +1,27 @@
+class Box{
+    int length;
+    int breadth;
+    int height;
+
+    Box(int l,int b,int h){
+        length=l;
+        breadth=b;//instance variables
+        height=h;
+    }
+
+    /*void setDim(int l,int b,int h){
+        length=l;
+        breadth=b;
+        height=h;
+    }*/
+    int volume(){//method
+        return length*breadth*height;
+    }
+}
+public class ClassDemo {
+    public static void main(String[] args) {
+        Box blackBox=new Box(2,9,5);
+       // blackBox.setDim(11,22,33);
+        System.out.println("vol of black box is "  + blackBox.volume());// calling method
+    }
+}
